8daef62c147b9eeb63fd6086be1c1aabe17a8994
Paulina Safar <paulina.safar@yahoo.com>
2021-11-05 10:02:03 +0100

    Submit Button

--- uno.js
+++ uno.js
@@ -5,38 +5,39 @@ myModal.show();
 const playerNames = [];
 const closeButton = document.getElementById('closeButton');
 const inputValues = document.getElementsByClassName('form-control');
+const playerName1 = document.getElementById('playername1');
+const playerName2 = document.getElementById('playername2');
+const playerName3 = document.getElementById('playername3');
+const playerName4 = document.getElementById('playername4');
 
 //Listening for player names + checking if all 4 names are inputed
-closeButton.addEventListener('click', addPlayerNames);
-function addPlayerNames() {
+document.getElementById('playerNamesForm').addEventListener('submit', function () {
     for (let i = 0; i < inputValues.length; ++i) {
         let playerName = inputValues[i].value;
         if (!playerName) {
             alert("Please enter a name for every Player!");
-            myModal.show();  //FUNKTIONIERT NICHT!
+            EventTarget.preventDefault();
+            break;
         } else {
             playerNames.push(playerName);
         }
     }
     console.log(playerNames);
     return playerNames;
-}
-
-//Listening for players with same names
-closeButton.addEventListener('click', function () {
-    
-    for (let i = 0; i < playerNames.length; ++i) {
-        let previousName = String(playerNames[i]);
-        for (let j = i + 1; i < playerNames.length; ++i) {
-            let nextName = String(playerNames[j]);
-            if (previousName.localeCompare(nextName) === 0) {
-                alert("Player " + String(playerNames[i]) + " already exists. Please enter a different player.");
-                myModal.show();  //FUNKTIONIERT NICHT!
-            }
-        }
-    }
 })
 
-// REQUIREMENTS:
-// ALL NAMES MUST BE ENTERED -> gemacht
-// NAMES MUST BE DIFFERENT -> nicht gemacht
+//Listening for players with same names
+document.getElementById('playerNamesForm').addEventListener('keyup', function () {
+        for(let i = 0; i < playerName1; ++i)
+})
+
+// for (let i = 0; i < playerNames.length; ++i) {
+//     let previousName = String(playerNames[i]);
+//     for (let j = i + 1; i < playerNames.length; ++i) {
+//         let nextName = String(playerNames[j]);
+//         if (previousName.localeCompare(nextName) === 0) {
+//             alert("Player " + String(playerNames[i]) + " already exists. Please enter a different player.");
+//             myModal.show();  //FUNKTIONIERT NICHT!
+//         }
+//     }
+// }
