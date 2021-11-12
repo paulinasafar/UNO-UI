2b2bcfefcc3f3bd60428f6ca0b7ee9fc5f3341d6
jules-testo <summer.julia@outlook.at>
2021-11-12 11:07:39 +0100

    pushed all files to jules branch

--- script/uno.js
+++ script/uno.js
@@ -7,6 +7,11 @@ const playerNamesHardCoded = ["Karo", "Mimi", "Steffi", "Lu"];
 const closeButton = document.getElementById('closeButton');
 const inputValues = document.getElementsByClassName('form-control');
 
+let gameID;
+let nextPlayer;
+let topCard;
+let allPlayers;
+
 // Listening for player names + checking if all 4 names are inputed
 // document.getElementById('playerNamesForm').addEventListener('submit', function (e) {
 //     e.preventDefault();
@@ -68,16 +73,13 @@ async function startGame() {
         allPlayers = result.Players;
         topCard = result.TopCard;
         placePlayersAndCards(allPlayers);
+
     } else {
         alert("HTTP-Error: " + response.status);
     }
 }
 
 startGame();
-let gameID;
-let nextPlayer;
-let topCard;
-let allPlayers;
 
 let playersAndCards = document.getElementById("players-and-cards").children;
 playersAndCards = Array.from(playersAndCards);
@@ -85,31 +87,48 @@ const player1Position = document.getElementById("player1");
 
 
 function placePlayersAndCards(players) {
-        document.getElementById("player1-name").innerHTML = players[0].Player;
-        document.getElementById("player1-points").innerHTML = players[0].Score;
-        document.getElementById("player1-allCards").innerHTML = players[0].Cards;
 
-        document.getElementById("player2-name").innerHTML = players[1].Player;
-        document.getElementById("player2-points").innerHTML = players[1].Score;
-        document.getElementById("player2-allCards").innerHTML = players[1].Cards;
 
-        document.getElementById("player3-name").innerHTML = players[2].Player;
-        document.getElementById("player3-points").innerHTML = players[2].Score;
-        document.getElementById("player3-allCards").innerHTML = players[2].Cards;
+    document.getElementById("player1-name").innerHTML = players[0].Player;
+    document.getElementById("player1-points").innerHTML = players[0].Score;
+    document.querySelector("#player1-allCards ul").append(getCards(players[0].Player));
 
-        document.getElementById("player4-name").innerHTML = players[3].Player;
-        document.getElementById("player4-points").innerHTML = players[3].Score;
-        document.getElementById("player4-allCards").innerHTML = players[3].Cards;
+    document.getElementById("player2-name").innerHTML = players[1].Player;
+    document.getElementById("player2-points").innerHTML = players[1].Score;
+    document.getElementById("player2-allCards").innerHTML = players[1].Cards;
+
+    document.getElementById("player3-name").innerHTML = players[2].Player;
+    document.getElementById("player3-points").innerHTML = players[2].Score;
+    document.getElementById("player3-allCards").innerHTML = players[2].Cards;
+
+    document.getElementById("player4-name").innerHTML = players[3].Player;
+    document.getElementById("player4-points").innerHTML = players[3].Score;
+    document.getElementById("player4-allCards").innerHTML = players[3].Cards;
 }
 
+const baseUrl = "http://nowaunoweb.azurewebsites.net/Content/Cards/";
 
 function createCards(card) {
     const li = document.createElement("li");
     const img = document.createElement("img");
-    // const card = makeCardFromInput(colorInput.value, numberInput.value);
-    img.src = `${baseUrl}${card}.png`;
-    li.appendChild(img);
+    const color = card.Color.slice(0, 1).toLowerCase();
+    const value = card.Value;
+    img.src = `${baseUrl}${color}${value}.png`;
+    return li;
+}
 
-    document.querySelector("#cards ul").appendChild(li);
-
+async function getCards(player) {
+    let response = await fetch("http://nowaunoweb.azurewebsites.net/api/Game/GetCards/" + gameID + "?playerName=" + player, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    });
+    if (response.ok) {
+        let result = await response.json();
+        console.log(result);
+        createCards(result.Cards);
+    } else {
+        alert("HTTP-Error: " + response.status);
+    }
 }
